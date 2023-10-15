da6eedfc34c543aa20631d41e83f7255467b647c
Ugo1000 <ugo.samuel.java@gmail.com>
2023-10-15 11:25:19 +0100

    list,set and dictionary comprehension

--- 100DaysCodingPython/DAY_31/Comprehensio.py
+++ 100DaysCodingPython/DAY_31/Comprehensio.py
@@ -2,19 +2,22 @@
 
 # list comprehension syntax  : new_list = [expression for item in iterable]
 
-squares = [x ** 2 for x in range(5)]
+squares = [x * 2 for x in range(1,5)]
 print(squares)
 
 # Dictionary Comprehensions : new_dict = {key_expression: value_expression for key, value in iterable}
 
-squares_dic = {x:  x**2 for x in range(5)}
-print(squares_dic)
+# squares_dic = {x:  x**x for x in range(5)}
+# print(squares_dic)
 
-# Set comprehension : new_set = {expression for item in iterable}
-even_num = {x for x in range(10) if x % 2 == 0}
-print(even_num)
+# # Set comprehension : new_set = {expression for item in iterable}
+# even_num = {x for x in range(10) if x % 2 == 0}
+# print(even_num)
 
-even_squares = [x**2 for x in range(10) if x % 2 == 0]
-print(even_squares)
+# even_squares = [x**2 for x in range(10) if x % 2 == 0]
+# print(even_squares)
 
+
+# name = ['sam', 'ugo', 'okoro']
+# for i in range()
 
