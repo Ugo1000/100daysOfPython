62cc2f5e206b4c2f60c80e25c8be5f541e890b40
Ugo1000 <ugo.samuel.java@gmail.com>
2024-01-24 05:55:52 +0100

    jpeg2png

--- 100DaysCodingPython/DAY_41/jpeg_to_pngConverter.py
+++ 100DaysCodingPython/DAY_41/jpeg_to_pngConverter.py
@@ -5,9 +5,50 @@ import os
 
 # grab the first and second arguments
 image_folder = sys.argv[1]
-out_put = sys.argv[2]
+out_put_folder = sys.argv[2]
 
 
 # check if they exits
-if not os.path.exists(out_put):
-    os.makedirs(out_put)
+if not os.path.exists(out_put_folder):
+    os.makedirs(out_put_folder)
+
+
+for filename in os.listdir(image_folder):
+    # open image
+    img = Image.open(f'{image_folder}{filename}')
+    # remove the extension
+    clean_name = os.path.splitext(filename)[0]
+    # save the image to the new folder
+    img.save(f'{out_put_folder}{clean_name}.png', format="PNG")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # img = Image.open(f'{image_folder}{filename}')  # open the image
+    # clean_name = os.path.splitext(filename)[0]  # remove the extension
+    # # save the image to the new folder
+    # img.save(f'{out_put_folder}{clean_name}.png', 'png')
+    # print('all done!')
